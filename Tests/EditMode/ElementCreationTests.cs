9263e77a5cfc0728d0efd25490ac19f8e823e167
Serhii Smirnov <8C61DAED-35CE80AE-3B7D85D0-D4D913C1-3CB71CB8>
2024-03-29 20:12:48 +0100

    updated element size test

--- Tests/EditMode/ElementCreationTests.cs
+++ Tests/EditMode/ElementCreationTests.cs
@@ -35,16 +35,19 @@ namespace MatchX.Engine.Tests.EditMode
 			var expectedSize1x1 = new uint2(1, 1);
 			var expectedSize2x1 = new uint2(2, 1);
 			var expectedSize1x2 = new uint2(1, 2);
+			var expectedSize2x2 = new uint2(2, 2);
 			
 			var entity1x1 = CreateDynamicElement(new int2(0, 1), GetShape1x1());
 			var entity2x1 = CreateDynamicElement(new int2(0, 2), GetShape2x1());
 			var entity1x2 = CreateDynamicElement(new int2(0, 3), GetShape1x2());
+			var entity2x2 = CreateDynamicElement(new int2(0, 4), GetShape2x2());
 
 			Update();
 
 			Assert.AreEqual(expectedSize1x1, EntityManager.GetComponentData<Element.Size>(entity1x1).Value);
 			Assert.AreEqual(expectedSize2x1, EntityManager.GetComponentData<Element.Size>(entity2x1).Value);
 			Assert.AreEqual(expectedSize1x2, EntityManager.GetComponentData<Element.Size>(entity1x2).Value);
+			Assert.AreEqual(expectedSize2x2, EntityManager.GetComponentData<Element.Size>(entity2x2).Value);
 		}
 	}
 
