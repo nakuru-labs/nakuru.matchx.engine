5361fad059fb333416c60d8035d409cbd75990fa
Serhii Smirnov <8C61DAED-35CE80AE-3B7D85D0-D4D913C1-3CB71CB8>
2024-04-06 12:05:47 +0200

    improved gravity system

--- Runtime/Systems/GravitySystem.cs
+++ Runtime/Systems/GravitySystem.cs
@@ -5,12 +5,12 @@ using Unity.Mathematics;
 
 namespace MatchX.Engine
 {
-	
+
 	public partial struct GravitySystem : ISystem
 	{
 		private EntityQuery _boardQuery;
 		private EntityQuery _elementsQuery;
-		
+
 		[BurstCompile]
 		public void OnCreate(ref SystemState state)
 		{
@@ -19,7 +19,7 @@ namespace MatchX.Engine
 
 			_elementsQuery = SystemAPI.QueryBuilder().WithAll<Element.Tag, Board.Position, Element.Shape, Element.Is.Dynamic>().Build();
 			state.RequireForUpdate(_elementsQuery);
-			
+
 			state.RequireForUpdate<EndSimulationEntityCommandBufferSystem.Singleton>();
 		}
 
@@ -31,7 +31,7 @@ namespace MatchX.Engine
 		{
 			var ecbSingleton = SystemAPI.GetSingleton<EndSimulationEntityCommandBufferSystem.Singleton>();
 			var ecb = ecbSingleton.CreateCommandBuffer(state.WorldUnmanaged);
-			
+
 			var boardSize = _boardQuery.GetSingleton<Board.Size>();
 			var boardGravity = _boardQuery.GetSingleton<Board.Gravity>();
 			var boardState = _boardQuery.GetSingletonBuffer<Board.CellState>();
@@ -43,8 +43,8 @@ namespace MatchX.Engine
 			var endingPositions = FindAllEndingPositions(ref state, boardSize, boardGravity);
 
 			foreach (var (positionRo, entity) in SystemAPI.Query<RefRO<Board.Position>>()
-			                                       .WithAll<Element.Tag>()
-			                                       .WithEntityAccess()) {
+			                                              .WithAll<Element.Tag>()
+			                                              .WithEntityAccess()) {
 				elementsMap.Add(positionRo.ValueRO.Value, entity);
 			}
 
@@ -60,29 +60,28 @@ namespace MatchX.Engine
 
 					isNextSlotFree = !stateCopy[nextSlotIndex];
 					previousPosition -= boardGravity.Value;
+
 					isOutOfBoardBounds = previousPosition.x < 0 || previousPosition.x >= boardSize.Width || previousPosition.y < 0
 					                     || previousPosition.y >= boardSize.Height;
 
 					if (isOutOfBoardBounds)
 						continue;
 
-					var previousSlotIndex = previousPosition.y * boardSize.Width + previousPosition.x;
-					var isPreviousSlotFree = !boardState[previousSlotIndex];
+					if (elementsMap.TryGetValue(previousPosition, out var elementEntity)) {
+						var previousSlotIndex = previousPosition.y * boardSize.Width + previousPosition.x;
+						var isPreviousSlotFree = !boardState[previousSlotIndex];
+						var shapesBuffer = state.EntityManager.GetBuffer<Element.Shape>(elementEntity);
+						var shapes = shapesBuffer.Reinterpret<int2>().ToNativeArray(Allocator.Temp);
 
-					// if the origin tile can fall
-					if (!isPreviousSlotFree && isNextSlotFree) {
-
-						if (elementsMap.TryGetValue(previousPosition, out var elementEntity)) {
-
-							var shapesBuffer = state.EntityManager.GetBuffer<Element.Shape>(elementEntity);
-							var shapes = shapesBuffer.Reinterpret<int2>().ToNativeArray(Allocator.Temp);
+						// if the origin tile can fall
+						if (!isPreviousSlotFree && isNextSlotFree) {
 							var allShapesCanFallDown = true;
-							
+
 							foreach (var shape in shapes) {
 								var shapePosition = previousPosition + shape;
 								var nextShapePosition = shapePosition + boardGravity.Value;
 								var slotIndex = nextShapePosition.y * boardSize.Width + nextShapePosition.x;
-				
+
 								// if it's a position of the element itself
 								if (shapes.Contains(shapePosition - nextShapePosition))
 									continue;
@@ -97,24 +96,24 @@ namespace MatchX.Engine
 								foreach (var shape in shapes) {
 									var shapePosition = previousPosition + shape;
 									var shapeSlotIndex = shapePosition.y * boardSize.Width + shapePosition.x;
-									
+
 									if (shapeSlotIndex >= 0 && shapeSlotIndex < stateCopy.Length)
 										stateCopy[shapeSlotIndex] = false;
 								}
-								
+
 								foreach (var shape in shapes) {
 									var shapePosition = nextPosition + shape;
 									var shapeSlotIndex = shapePosition.y * boardSize.Width + shapePosition.x;
 									stateCopy[shapeSlotIndex] = true;
-									
+
 									if (shapeSlotIndex >= 0 && shapeSlotIndex < stateCopy.Length)
 										stateCopy[shapeSlotIndex] = true;
 								}
-								
+
 								var elementId = SystemAPI.GetComponentRO<Element.Id>(elementEntity);
 								var positionRw = SystemAPI.GetComponentRW<Board.Position>(elementEntity);
 								positionRw.ValueRW.Value = nextPosition;
-								
+
 								// create output
 								var outputEntity = ecb.CreateEntity();
 								ecb.SetName(outputEntity, $"Output<{nameof(EngineOutput.ElementMoved)}>");
@@ -127,18 +126,19 @@ namespace MatchX.Engine
 					}
 
 					counter++;
-
 				} while (!isOutOfBoardBounds || counter >= 100);
 			}
 		}
 
-		private bool CanElementBeMoved(int2 elementPosition, Board.Size boardSize, Board.Gravity boardGravity, NativeArray<int2> elementShapes, NativeArray<bool> boardState)
+		private bool CanElementBeMoved(
+			int2 elementPosition, Board.Size boardSize, Board.Gravity boardGravity, NativeArray<int2> elementShapes,
+			NativeArray<bool> boardState)
 		{
 			foreach (var shape in elementShapes) {
 				var shapePosition = elementPosition + shape;
 				var nextShapePosition = shapePosition + boardGravity.Value;
 				var slotIndex = nextShapePosition.y * boardSize.Width + nextShapePosition.x;
-				
+
 				// if it's a position of the element itself
 				if (elementShapes.Contains(nextShapePosition - shapePosition))
 					continue;
@@ -153,10 +153,10 @@ namespace MatchX.Engine
 		private NativeList<int2> FindAllEndingPositions(ref SystemState state, Board.Size boardSize, Board.Gravity boardGravity)
 		{
 			var result = new NativeList<int2>(boardSize.Width * boardSize.Height, Allocator.Temp);
-			
+
 			foreach (var slotPosition in SystemAPI.Query<RefRO<Board.Position>>().WithAll<Slot.Tag>()) {
 				var nextPosition = slotPosition.ValueRO.Value + boardGravity.Value;
-				
+
 				// if nextPosition is out of board bounds then it's an ending position
 				// Ending position means that the element can't be moved anywhere further by the gravity 
 				if (nextPosition.x < 0 || nextPosition.x >= boardSize.Width)
